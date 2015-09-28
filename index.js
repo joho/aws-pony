fe1b36d9586fc2283e1633eb49c663d21af70b05
John Barton (joho) <jrbarton@gmail.com>
2015-09-28 14:07:34 +1000

    Fix path to wallpaper and re-release.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var path = require('path');
 
 
 if (process.env.AWS_SECRET_ACCESS_KEY) {
-  var pathToWallpaper = path.resolve(__dirname.split(path.sep).pop() + 'my_little_pony_wallpaper_by_theshadowstone-d7iyp7s.png');
+  var pathToWallpaper = path.resolve(__dirname + '/my_little_pony_wallpaper_by_theshadowstone-d7iyp7s.png');
   var desktopCommand = 'osascript -e \'tell application "Finder" to set desktop picture to "' + pathToWallpaper + '" as POSIX file\'';
   var child = exec(desktopCommand, function(err, stdout, stderr) {
     console.log("You could have just leaked your AWS creds");
