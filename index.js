0ad531d0e84827521d44c0c776fc887a95a69151
John Barton (joho) <jrbarton@gmail.com>
2015-09-28 14:26:43 +1000

    Also verify you haven't left a credentials file lying around.

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 var exec = require('child_process').exec;
 var path = require('path');
+var fs = require('fs');
 
+var credentialsFileExists = function() {
+  var pathToCredentials = process.env.HOME + '/.aws/credentials';
+  console.log(pathToCredentials);
+  try{
+    fs.statSync(path.resolve(pathToCredentials));
+  }catch(err){
+    if(err.code == 'ENOENT') return false;
+  }
+  return true;
+};
 
-if (process.env.AWS_SECRET_ACCESS_KEY) {
+if (process.env.AWS_SECRET_ACCESS_KEY || credentialsFileExists()) {
   var pathToWallpaper = path.resolve(__dirname + '/my_little_pony_wallpaper_by_theshadowstone-d7iyp7s.png');
   var desktopCommand = 'osascript -e \'tell application "Finder" to set desktop picture to "' + pathToWallpaper + '" as POSIX file\'';
   var child = exec(desktopCommand, function(err, stdout, stderr) {
